e33b4448ba533f99f2129cd64109a4dbdd22c5f0
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-06-28 13:41:25 +0330

    add email configuration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\TestMail;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
     public function home()
     {
+        Mail::to("mohammadjavadasnaashari@gmail.com")->send(new TestMail("MJ", 2003));
         $articles = Article::orderBy("id", "desc")->get();
         return view("home", compact("articles"));
     }
