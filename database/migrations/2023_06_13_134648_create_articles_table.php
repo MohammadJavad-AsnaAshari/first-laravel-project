20a1c40f444240a793448d3752fb79c2b6df1717
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-07-01 16:16:20 +0330

    add user_id column to articles for one to many relationship

--- database/migrations/2023_06_13_134648_create_articles_table.php
+++ database/migrations/2023_06_13_134648_create_articles_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->references("id")->on("users")->onDelete("cascade");
             $table->string("title", 50);
             $table->string("slug", 100);
             $table->text("body");
